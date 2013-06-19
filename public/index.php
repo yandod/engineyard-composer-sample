1ae78022e9715646b59bf0eb14811da88e6e902b
Yusuke Ando <yandod@gmail.com>
2013-06-19 18:27:22 +0900

    up

--- public/index.php
+++ public/index.php
@@ -1,6 +1,10 @@
+<pre>
 <?php
-require '../vendor/autoload.php'
+require '../vendor/autoload.php';
 
 use Monolog\Logger;
-var_dump(get_class_methods('Logger'));
+$obj = new Logger('sample');
+var_dump(get_class($obj));
+var_dump($obj);
+?></pre>
 
